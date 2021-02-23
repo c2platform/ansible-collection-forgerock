a918bd834996a1a6937fda08bf285b896b021c2e
onknows <onno@onknows.com>
2021-02-23 10:03:20 +0100

    opends_cmd with support duplicate switches

--- plugins/filter/ds_filters.py
+++ plugins/filter/ds_filters.py
@@ -5,11 +5,15 @@ __metaclass__ = type
 
 from ansible.errors import AnsibleFilterError
 
-# Return command switches for OpenDS
+# Return command switches for ForgeRock CLI utilities
 def opends_cmd(cmd_config):
   cmd_line = ''
   for key in cmd_config:
-    cmd_line += ' --{} {}'.format(key,cmd_config[key])
+    if isinstance(cmd_config[key], list):
+      for v in cmd_config[key]:
+        cmd_line += ' --{} {}'.format(key,v)
+    else:
+      cmd_line += ' --{} {}'.format(key,cmd_config[key])
   return cmd_line
 
 class FilterModule(object):
