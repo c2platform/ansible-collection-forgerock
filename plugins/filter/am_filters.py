17d7c10b8c0d908a8e9c03889b46b3e2a8a5b65b
onknows <onno@onknows.com>
2021-07-05 14:14:49 +0200

    am refactoring

--- plugins/filter/am_filters.py
+++ plugins/filter/am_filters.py
@@ -3,11 +3,6 @@ from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 
-def amster_script_index(am_configure_item, am_configure):
-    idx = am_configure.index(am_configure_item)
-    return str(idx).zfill(2)
-
-
 def amster_param_quotes(var):
     if 'string' in var:
         if var['string'] is False:
@@ -16,7 +11,20 @@ def amster_param_quotes(var):
 
 
 def amster_script_header(item):
-    return "Script: {}\nTemplate: {}".format(item['script'], item['template'])
+    if 'template' in item:
+        return "Script: {}\nTemplate: {}".format(
+            item['script'], item['template'])
+    return "Subscript: {}".format(item)
+
+
+# Return notify for script or None
+def amster_script_notify(script, am_configure, am_amster_templates):
+    for itm in am_configure:
+        if itm['script'] == script:
+            tpl = am_amster_templates[itm['template']]
+            if 'notify' in tpl:
+                return tpl['notify']
+    return 'empty-am-hander'
 
 
 class FilterModule(object):
@@ -25,6 +33,6 @@ class FilterModule(object):
     def filters(self):
         return {
             'amster_script_header': amster_script_header,
-            'amster_param_quotes': amster_param_quotes,
-            'amster_script_index': amster_script_index
+            'amster_script_notify': amster_script_notify,
+            'amster_param_quotes': amster_param_quotes
         }
--- plugins/modules/amster_script_prepare.py
+++ plugins/modules/amster_script_prepare.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 
 from ansible.module_utils.basic import *
-# import glob, os, hashlib, uuid
+import glob
+import os
 
 
 def script(step, name, template):
@@ -9,6 +10,15 @@ def script(step, name, template):
     return a.lower().replace('_', '-').replace(' ', '-')
 
 
+# Return the old name of the script ( so it can be removed )
+def obsolete_script_name(step, name, template, dest):
+    current_script = script(step, name, template)
+    for s in glob.glob("{}/s{}-*".format(dest, step)):
+        if s != current_script:
+            return s
+
+
+# Set additional facts in am_configure: step, script, script-obsolete
 def amster_script_prepare(data):
     fcts = {}
     fcts['am_configure'] = data['configure']
@@ -16,13 +26,19 @@ def amster_script_prepare(data):
     for c in fcts['am_configure']:
         c['step'] = str(i).zfill(2)
         c['script'] = script(c['step'], c['name'], c['template'])
-        # c['script'] = amster_script_name() # TODO
+        osn = obsolete_script_name(
+            c['step'], c['name'], c['template'], data['dest'])
+        if osn is not None:
+            bn = os.path.basename(osn)
+            if bn != c['script']:
+                c['script-obsolete'] = bn
         i += 1
     return True, fcts, str(i) + " Amster scripts prepared"
 
 
 def main():
     fields = {
+        "dest": {"required": True, "type": "str"},
         "configure": {"required": True, "type": "list"},
         "templates": {"required": True, "type": "dict"}}
     module = AnsibleModule(argument_spec=fields)
