e349217d01896a3914464079ba7db2078bac579a
onknows <onno@onknows.com>
2022-04-04 13:17:29 +0200

    0.1.2 fix bug ds_filters.py

--- plugins/filter/ds_filters.py
+++ plugins/filter/ds_filters.py
@@ -273,7 +273,7 @@ def ds_modify_when(item, not_if_results, only_if_results):
             not_if = True
     if 'enabled' in item:
         if not item['enabled']:
-            not_if = False
+            not_if = True
     # pprint({'not_if': not_if, 'only_if': only_if})
     return (not not_if) and only_if
 
